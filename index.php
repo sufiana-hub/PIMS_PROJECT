84e4217d6a2140c44596f664bff8a29adfbd547c
sufiana-hub <sufianabaharom@gmail.com>
2026-06-13 16:51:05 +0800

    s

--- index.php
+++ index.php
@@ -39,19 +39,20 @@
       <!-- Uncomment below if you prefer to use an image logo -->
       <a href="index.html" class="logo"><img src="assets\img\pimslogo.png" alt="" class="img-fluid"></a>
 
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="index.php">Utama</a></li>
-          <li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
-          <li><a class="nav-link scrollto" href="#services">Menu</a></li>
-          <li><a class="nav-link scrollto o" href="picture.php">Galeri Gambar PIMS</a></li>
-          <li><a class="nav-link scrollto" href="infaq.php">Sumbangan</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Hubungi Kami</a></li>
-          <li><a class="getstarted scrollto" href="signup.php" id="signup-btn">Daftar Masuk</a></li>
-          <li><a class="getstarted scrollto" href="login.php" id="signup-btn">Log Masuk</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
+<nav id="navbar" class="navbar">
+  <ul>
+    <li><a class="nav-link scrollto active" href="index.php">Utama</a></li>
+    <li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
+    <li><a class="nav-link scrollto" href="#services">Menu</a></li>
+    <li><a class="nav-link scrollto o" href="picture.php">Galeri Gambar PIMS</a></li>
+    <li><a class="nav-link scrollto" href="infaq.php">Sumbangan</a></li>
+    <li><a class="nav-link scrollto" href="#contact">Hubungi Kami</a></li>
+    <!-- FIXED BELOW: Removed the extra 'a' and ensured lowercase -->
+    <li><a class="getstarted scrollto" href="signup.php" id="signup-btn">Daftar Masuk</a></li>
+    <li><a class="getstarted scrollto" href="login.php" id="login-btn">Log Masuk</a></li>
+  </ul>
+  <i class="bi bi-list mobile-nav-toggle"></i>
+</nav><!-- .navbar -->
 
       <script>
     // Add event listeners to the "Login" button
